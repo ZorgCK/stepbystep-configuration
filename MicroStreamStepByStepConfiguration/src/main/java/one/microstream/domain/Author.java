84b0edebcaf204fee1bccdd6cf0e8d148873e594
Kümmel Christian <c.kuemmel@microstream.one>
2021-03-31 13:02:22 +0200

    Classes added: Author, Book; Registered in root class

---
+++ MicroStreamStepByStepConfiguration/src/main/java/one/microstream/domain/Author.java
@@ -0,0 +1,39 @@
+package one.microstream.domain;
+
+public class Author
+{
+	private String	mail;
+	private String	firsrname;
+	private String	lastname;
+	
+	public String getMail()
+	{
+		return mail;
+	}
+	
+	public void setMail(String mail)
+	{
+		this.mail = mail;
+	}
+	
+	public String getFirsrname()
+	{
+		return firsrname;
+	}
+	
+	public void setFirsrname(String firsrname)
+	{
+		this.firsrname = firsrname;
+	}
+	
+	public String getLastname()
+	{
+		return lastname;
+	}
+	
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+	
+}
---
+++ MicroStreamStepByStepConfiguration/src/main/java/one/microstream/domain/Book.java
@@ -0,0 +1,39 @@
+package one.microstream.domain;
+
+public class Book
+{
+	private String	isbn;
+	private String	name;
+	private Author	author;
+	
+	public String getIsbn()
+	{
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn)
+	{
+		this.isbn = isbn;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public Author getAuthor()
+	{
+		return author;
+	}
+	
+	public void setAuthor(Author author)
+	{
+		this.author = author;
+	}
+	
+}
