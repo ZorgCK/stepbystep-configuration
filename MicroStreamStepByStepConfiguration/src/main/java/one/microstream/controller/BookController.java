8982e26c4f9dcd372bec44aa43db29d3430fbf9d
Kümmel Christian <c.kuemmel@microstream.one>
2021-03-31 14:18:31 +0200

    Added two controllers

--- MicroStreamStepByStepConfiguration/src/main/java/one/microstream/controller/BookController.java
+++ MicroStreamStepByStepConfiguration/src/main/java/one/microstream/controller/BookController.java
@@ -1,10 +1,16 @@
 package one.microstream.controller;
 
+import java.util.List;
+
+import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
+import one.microstream.DB;
 import one.microstream.domain.Author;
+import one.microstream.domain.Book;
 
 
 @Controller("/books")
@@ -12,9 +18,25 @@ public class BookController
 {
 	@Get("/create")
 	@Produces(MediaType.TEXT_PLAIN)
-	public void createBooks()
+	public HttpResponse<?> createBooks()
 	{
-		Author author = new Author();
+		Author author1 = new Author("Charlotte", "Link", "c.link@example.com");
+		Author author2 = new Author("Dan", "Brown", "d.brown@example.com");
 		
+		Book book = new Book("9783732542215", "Origin", author2);
+		Book book2 = new Book("9783732542215", "Diabolus", author2);
+		Book book3 = new Book("978-3-7341-0742-9", "Die Suche", author1);
+		Book book4 = new Book("978-3-7341-0522-7", "Die Erscheinung", author1);
+		
+		DB.root.getBooks().addAll(CollectionUtils.setOf(book, book2, book3, book4));
+		DB.storageManager.store(DB.root.getBooks());
+		
+		return HttpResponse.ok("Books successfully created!");
+	}
+	
+	@Get
+	public List<Book> getBook()
+	{
+		return DB.root.getBooks();
 	}
 }
