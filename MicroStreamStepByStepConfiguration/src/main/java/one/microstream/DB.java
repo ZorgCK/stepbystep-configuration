5a4828ee51b096b25b3f0f31088609ac40db52dd
Kümmel Christian <c.kuemmel@microstream.one>
2022-11-07 22:48:42 +0100

    Upgrade microstream framework, change to builder storage

--- MicroStreamStepByStepConfiguration/src/main/java/one/microstream/DB.java
+++ MicroStreamStepByStepConfiguration/src/main/java/one/microstream/DB.java
@@ -1,10 +1,5 @@
 package one.microstream;
 
-import java.net.URL;
-import java.util.Optional;
-
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.core.io.scan.ClassPathResourceLoader;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
 
@@ -15,11 +10,10 @@ public class DB
 	public final static DataRoot			root	= new DataRoot();
 	
 	static
-	{
-		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> resource = loader.getResource("microstream.xml");
-		
-		storageManager = EmbeddedStorageConfiguration.load(
-			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root).start();
+	{		
+		storageManager = EmbeddedStorageConfiguration.Builder()
+			.setStorageDirectory("data")
+			.createEmbeddedStorageFoundation()
+			.createEmbeddedStorageManager(root).start();
 	}
 }
